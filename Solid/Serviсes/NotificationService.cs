a67a1fc5b152d502667eac5e8af4b1283fe2c223
srgo211 <novikovsn87@gmail.com>
2023-12-13 19:35:45 +0400

    Релизовал сервис уведомлений + генерация рандом чисел

---
+++ Solid/Serviсes/NotificationService.cs
@@ -0,0 +1,19 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace Solid.Serviсes;
+
+internal class NotificationService : INotificationService
+{
+    public void InfoToLog(string text) => Info(text, ConsoleColor.Green);
+    public void WarringToLog(string text) => Info(text, ConsoleColor.Yellow);
+    public void ErrorToLog(string text) => Info(text, ConsoleColor.Red);
+
+    private void Info(string text, ConsoleColor foregroundColor)
+    {
+        if(String.IsNullOrWhiteSpace(text)) return;
+
+        Console.ForegroundColor = foregroundColor;
+        Console.WriteLine(text);
+        Console.ResetColor();
+    }
+}
---
+++ Solid/Serviсes/NumberGeneratorService.cs
@@ -0,0 +1,10 @@
+﻿namespace Solid.Serviсes;
+
+internal class NumberGeneratorService : INumberGeneratorServiсe
+{
+    public int GenerateRandomNumber(int minValue, int maxValue)
+    {
+        Random rnd = new Random();
+        return rnd.Next(minValue, maxValue);
+    }
+}
